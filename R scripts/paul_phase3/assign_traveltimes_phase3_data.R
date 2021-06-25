e364669706e777c544fd4bae2bfb905fe19bb040
Paul Delamater <delamate@pauls-mac-pro.dhcp.unc.edu>
2021-06-25 11:11:32 -0400

    Initial commit

---
+++ R scripts/paul_phase3/assign_traveltimes_phase3_data.R
@@ -0,0 +1,51 @@
+## Code to assign travel time to nearest Level II clinic to
+## Phase-3 location data from Boyce Uganda Project (immunization)
+
+## Libraries
+library(tidyverse)
+library(sf)
+library(raster)
+
+## Read cleaned version of data
+dat <- read_csv("../../output/BoyceGatesVaccineAcc_DATA_2021-04-20_0744_clean_geo.csv")
+
+## Convert to sf object
+dat <- st_as_sf(dat %>% as.data.frame(),
+                coords = c("LONG", "LAT"),
+                crs = 4326)
+
+## Subset to only id variable
+dat <- dat[,1]
+
+## Read in travel time, walking time
+walk.time <- raster("../../data/travel_time/Cost_minutes_Level_Il_III_walkonly.img")
+
+# Quick Map
+# tm_shape(walk.time) +
+#   tm_raster(breaks = c(-Inf, seq(15, 120, 15), Inf)) +
+# tm_shape(dat) +
+#   tm_basemap("CartoDB.Voyager") +
+#   tm_dots(size = 0.01)
+
+## Extract values from walking time to data
+dat$WALKTIME_LII <- extract(walk.time,
+                            dat)
+
+
+## Read in travel time, walking plus motorcycle time
+walk.motorc.time <- raster("../../data/travel_time/Cost_minutes_Level_III_walkmotorcycle.img")
+
+# Quick Map
+# tm_shape(walk.motorc.time) +
+#   tm_raster(breaks = c(-Inf, seq(15, 120, 15), Inf)) +
+# tm_shape(dat) +
+#   tm_basemap("CartoDB.Voyager") +
+#   tm_dots(size = 0.01)
+
+## Extract values from walking time to data
+dat$WLKMOTTIME_LIII <- extract(walk.motorc.time,
+                               dat)
+
+## Write out just tabular information
+write_csv(st_drop_geometry(dat),
+          "../../output/BoyceGatesVaccineAcc_DATA_2021-04-20_0744_clean_geo_traveltimes.csv")
